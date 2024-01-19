4502bf06c70b4ba23c7157ffa7a4e744e865a1b7
Felipe <felipesouzagomes21@gmail.com>
2024-01-18 21:52:09 -0300

    Criando entidade Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +22,9 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -45,11 +51,17 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(Id);
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -62,7 +74,6 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(Id, other.Id);
 	}
-	
-	
+
 
 }
